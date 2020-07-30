5cd697d57c4ecb7fe939e5df43532723463781a5
juanma <jmlpdr.93@gmail.com>
2020-07-30 21:33:20 +0100

    :beers: first commit, first card

---
+++ apps/flash-cards/src/app/flash/flash.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { IFlash } from './flash.model';
+
+@Component({
+  selector: 'app-flash',
+  templateUrl: './flash.component.html',
+  styleUrls: ['./flash.component.css']
+})
+export class FlashComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  @Input() flash:IFlash = {
+    id: 1,
+    question: 'React to Angular',
+    answer: 'No Reaction :)',
+    show: false,
+  };
+
+}
