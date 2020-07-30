29392b5eeceda5753db620adbbc068d07433cc05
juanma <jmlpdr.93@gmail.com>
2020-07-30 21:52:46 +0100

    :sparkles: add array of components

--- apps/flash-cards/src/app/app.component.ts
+++ apps/flash-cards/src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
+import { IFlash } from './flash/flash.model';
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 10000);
+}
 
 @Component({
   selector: 'flash-cards-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
   title = 'flash-cards';
+  flashs: IFlash[] = [{
+    question: 'Question 1',
+    answer: 'Answer 1',
+    show: false,
+    id: getRandomNumber(),
+  }, {
+      question: 'Question 2',
+      answer: 'Answer 2',
+      show: false,
+      id: getRandomNumber(),
+  }, {
+      question: 'Question 3',
+      answer: 'Answer 3',
+      show: false,
+      id: getRandomNumber(),
+  }];
+
+  trackByFlashId( index, flash) {
+    return flash.id;
+  }
+
 }
